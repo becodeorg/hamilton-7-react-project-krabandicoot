86aaf709b2ad184c82aced53d76189a4485d87a3
Khadja <khadjapaux.contact@gmail.com>
2022-11-25 10:58:00 +0100

    Grabbing data

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+// import { useState } from "react";
+import "./App.css";
+import Submit from "./components/Submit.jsx";
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body class="w-full overflow-scroll h-screen bg-gradient-to-r from-blue-100 via-purple-100 to-pink-100 p-4 flex items-center justify-center" >
+      <div>
+        <Submit />
+        
+      </div>
+    </body>
   );
 }
 
